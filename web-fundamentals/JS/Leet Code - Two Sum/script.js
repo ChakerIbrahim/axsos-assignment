2d0ad0429957f3c4f2e1410763f6e1e9a2095b42
ChakerIbrahim <chaker703@hotmail.com>
2026-03-17 13:11:15 +0200

    changes

--- web-fundamentals/JS/Leet Code - Two Sum/script.js
+++ web-fundamentals/JS/Leet Code - Two Sum/script.js
@@ -2,11 +2,12 @@
 
 function twoSum (nums,target){
     for(var i=0 ; i<nums.length ; i++){
-        for(var j=1 ; j<nums.length ; j++){
+        for(var j=i+1 ; j<nums.length ; j++){
             if(nums[i]+nums[j]==target)
                 return [i,j];
         }
     }
+                return [];
 }
 
-console.log(twoSum([3,2,4],6));
+console.log(twoSum([4,3,1],6));
