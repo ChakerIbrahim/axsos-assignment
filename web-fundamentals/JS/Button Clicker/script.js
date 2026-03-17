40494697bd5f1102e637d10af12309c8e1f77e8e
ChakerIbrahim <chaker703@hotmail.com>
2026-03-17 10:19:52 +0200

    click

---
+++ web-fundamentals/JS/Button Clicker/script.js
@@ -0,0 +1,11 @@
+function turnOff(element){
+    if(element.innerText == "Logout"){
+        element.innerText = "Login";
+    }
+    else{
+        element.innerText = "Logout";
+    }
+}   
+function hide(element){
+    element.remove();
+}
