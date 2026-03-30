9cacc21675ac5e3dbe20e96843f978e2b9fe7c97
ChakerIbrahim <chaker703@hotmail.com>
2026-03-30 15:54:10 +0300

    making connecions

--- web-fundamentals/JS/Likes part2/script.js
+++ web-fundamentals/JS/Likes part2/script.js
@@ -1,14 +1,10 @@
-var lik = document.querySelector(".likes1")
-function likes(element){
+function likes(num){
+
+    console.log(num)
+    var lik = document.querySelector('.likes'+num);
+    console.log(lik)
     lik.innerText++; 
-} 
-var lik1 = document.querySelector(".likes2")
-function likes1(element){
-    lik1.innerText++; 
-    
-} 
-var lik3= document.querySelector(".likes3")
-function likes2(element){
-    lik3.innerText++; 
-    
-} 
+
+}
+
+
