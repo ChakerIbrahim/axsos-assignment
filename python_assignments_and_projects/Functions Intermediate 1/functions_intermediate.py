7b1857ab7c7c0f8e10f88d0b845275076e8d0907
ChakerIbrahim <chaker703@hotmail.com>
2026-05-05 22:40:54 +0300

    first

---
+++ python_assignments_and_projects/Functions Intermediate 1/functions_intermediate.py
@@ -0,0 +1,19 @@
+import random
+
+def randInt(min=0, max=100):
+    
+    if max < 0:
+        max = 0
+    if min > max:
+        min, max = max, min  
+
+    num = round(random.random() * (max - min) + min)
+    return num
+
+print(randInt())                 
+print(randInt(max=50))            
+print(randInt(min=50))            
+print(randInt(min=50, max=500))  
+
+print(randInt(min=100, max=10))  
+print(randInt(max=-50))           
