52efee267b4154fcce38eade552143f8d2f69458
ChakerIbrahim <chaker703@hotmail.com>
2026-07-24 14:00:09 +0300

    week1mern

--- MERN/MERN Assignments/ninja.js
+++ MERN/MERN Assignments/ninja.js
@@ -1,28 +1,47 @@
 class Ninja {
-  constructor(name, health) {
-    ((this.name = name),
-    (this.health = health),
-    (this.speed = 3),
-    (this.strength = 3));
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    this.speed = 3;
+    this.strength = 3;
   }
 
   sayName() {
-    console.log(`The ninja name is ${ninja1.name}`);
+    console.log(`The ninja name is ${this.name}`);
   }
 
   showStats() {
     console.log(
-      `The ninja name is ${ninja1.name}  The ninja Strength is ${ninja1.strength}  The ninja speed is ${ninja1.speed}  The Ninja health is ${ninja1.health}`,
+      `Name: ${this.name}, Health: ${this.health}, Speed: ${this.speed}, Strength: ${this.strength}`
     );
   }
 
   drinkSake() {
-    this.health =+ 10;
+    this.health += 10;
   }
-  
 }
+
+class Sensei extends Ninja {
+  constructor(name) {
+    super(name);
+    this.speed = 10;
+    this.strength = 10;
+    this.health = 200;
+    this.wisdom = 10;
+  }
+
+  speakWisdom() {
+    this.drinkSake();
+    console.log("What one programmer can do in one month, two programmers can do in two months.");
+  }
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.sayName();
+superSensei.speakWisdom();
+superSensei.showStats(); // Health: 210 ✓
+
 const ninja1 = new Ninja("Hyabusa");
 ninja1.sayName();
 ninja1.drinkSake();
-ninja1.showStats();
-
+ninja1.showStats(); // Health: 110
